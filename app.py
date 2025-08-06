7ba77fc56dd9160b67e1d216faf976dd800bb309
raulkm108 <raulkm.civil@gmail.com>
2025-08-06 17:26:12 -0300

    Added secret key and communication with the db

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 db = SQLAlchemy(app)
 
 @app.route("/hello-world", methods=["GET"])
