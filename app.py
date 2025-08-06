168d8c26e0498f550f44d8b194c544eef2873966
raulkm108 <raulkm.civil@gmail.com>
2025-08-06 17:22:54 -0300

    Added a function in the route hello world

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 @app.route("", methods=["GET"])
+def hello_world ():
+    return "Hello world"
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(debug=True)
