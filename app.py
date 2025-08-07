a0d6af48ddbff8740c01009c027aa9c4d5ed82ef
raulkm108 <raulkm.civil@gmail.com>
2025-08-07 10:55:48 -0300

    Starting db passing the app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-
+db.init_app(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world ():
