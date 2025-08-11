7a86ea2c93d609c415262c7fc9dbb00e29724d72
raulkm108 <raulkm.civil@gmail.com>
2025-08-11 08:46:50 -0300

    Importing login_manager
    Importing login manager from flask_login, creating the variable login_manager in the loginmanager, and initializing the app inside it

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from database import db
 from models.user import User
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world ():
