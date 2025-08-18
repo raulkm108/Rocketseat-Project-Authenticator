e88a64186d92cde664032e79eb735e038195db83
raulkm108 <raulkm.civil@gmail.com>
2025-08-18 10:06:40 -0300

    Creating load_user method
    Method that searches and reaches the saved usernames and passwords

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ login_manager.init_app(app)
 
 login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route("/login", methods=["POST"])
 def login ():
     data = request.json
