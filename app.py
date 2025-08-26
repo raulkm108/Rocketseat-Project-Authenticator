1066c5df908e3a87aa0186586a9881e5fd4cca98
raulkm108 <raulkm.civil@gmail.com>
2025-08-26 18:42:08 -0300

    Importing cbrypt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from database import db
 from models.user import User
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 
 app = Flask(__name__)
